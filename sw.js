ef387fd325f40347731b412e29272811f44cd526
SURAJ K P <suraj@TXGCALICUT.COM>
2026-06-10 23:33:43 +0530

    Force cache refresh v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'gol-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'gol-v3';
+const FILES = ['/', '/index.html', '/manifest.json', '/js/matches.js', '/js/app.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
