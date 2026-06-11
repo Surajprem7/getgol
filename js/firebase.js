5127213aaec5f28bd41a24072e48d8907a329f46
SURAJ K P <suraj@TXGCALICUT.COM>
2026-06-11 14:43:25 +0530

    Add Firebase SDK and real-time predictions

---
+++ js/firebase.js
@@ -0,0 +1,29 @@
+// Firebase configuration for Gol!
+const firebaseConfig = {
+  apiKey: "AIzaSyD8A3QjR4y846DnoKppoF6x6WMUkSUm2-c",
+  authDomain: "getgol7.firebaseapp.com",
+  databaseURL: "https://getgol7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "getgol7",
+  storageBucket: "getgol7.firebasestorage.app",
+  messagingSenderId: "348385567552",
+  appId: "1:348385567552:web:6044919c6d0163b7a110a5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+// Save prediction to Firebase
+function savePrediction(matchId, pick) {
+  const ref = db.ref('predictions/' + matchId + '/' + pick);
+  ref.transaction(count => (count || 0) + 1);
+  localStorage.setItem('pred_' + matchId, pick);
+}
+
+// Get predictions from Firebase
+function getPredictions(matchId, callback) {
+  db.ref('predictions/' + matchId).on('value', snapshot => {
+    const data = snapshot.val() || {};
+    callback(data);
+  });
+}
